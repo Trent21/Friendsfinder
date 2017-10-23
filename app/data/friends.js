b37320b03367f629c7e4144aea8938e59dfa4e46
Trent21 <tmitch.wall@gmail.com>
2017-10-22 23:20:42 -0400

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,84 @@
+var friends = [
+    
+    {
+        name: "Robbie",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Sigmundur_Dav%C3%AD%C3%B0_Gunnlaugsson_2016_%28cropped_resized%29.jpg/170px-Sigmundur_Dav%C3%AD%C3%B0_Gunnlaugsson_2016_%28cropped_resized%29.jpg",
+        scores: [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "1",
+        "2",
+        "5",
+        "2",
+        "1"
+                ]
+    },
+    {
+        name: "Bill",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDKqajtQOoIWrS82sxC7zABgpmTrElmlZ84fbbcO8nMBGIPAat",
+        scores: [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "1",
+        "2",
+        "5",
+        "2",
+        "1"
+                ]
+    },
+    {
+        name: "Jack",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Sigur%C3%B0ur_Ingi_J%C3%B3hannsson_2016_%28cropped%29.png/220px-Sigur%C3%B0ur_Ingi_J%C3%B3hannsson_2016_%28cropped%29.png",
+        scores: [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "1",
+        "2",
+        "5",
+        "2",
+        "1"
+                ]
+    },
+    {
+        name: "Jill",
+        photo: "https://i.pinimg.com/736x/2f/a0/6f/2fa06f54cedda213d6b1aefae26e3c3c--redheads-haircolor.jpg",
+        scores: [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "1",
+        "2",
+        "5",
+        "2",
+        "1"
+                ]
+    },
+    {
+        name: "Johnny",
+        photo: "https://campaignforsocialscience.org.uk/wp-content/uploads/2015/02/James-Wilsdon-214x300.jpg",
+        scores: [
+        "4",
+        "5",
+        "1",
+        "3",
+        "2",
+        "1",
+        "2",
+        "5",
+        "2",
+        "1"
+                ]
+    }
+]
+module.exports = friends;
